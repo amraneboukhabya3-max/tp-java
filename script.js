0b20e78d77c2d8d67fdd180f7faa41ae46750a82
AMRANE <amraneboukhabya3@gmail.com>
2025-11-04 14:53:55 +0000

    feat: Maîtrise des variables et conditions

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
  console.log("Laboratoire prêt !");
  const nom = "Amrane";
-let age =16;
-
+let age = 18;
+ // nom = "palala";
+ console.log(age);
+const motDePasseAttendu = "12345678";
+let motDePasseUtilisateur = "12345678"; // Changez cette valeur pour tester
+if (motDePasseUtilisateur === motDePasseAttendu) {
+    console.log("Accès autorisé. Bienvenue !");
+} else if (motDePasseUtilisateur.length <8) {
+    console.log("Accès refusé. Votre mot de passe est trop court (8 caractères min).");
+} else {
+    console.log("Accès refusé. Mot de passe incorrect.");
+}
